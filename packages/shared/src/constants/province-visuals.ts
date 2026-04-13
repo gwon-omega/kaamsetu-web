65df9aff091a47ff750a0f2624121d93751e6706
gwon-omega <jeewanbhatt.bedcsit2081@fwu.edu.np>
2026-04-13 15:17:09 +0545

    fix: sync province visuals in web package

--- packages/shared/src/constants/province-visuals.ts
+++ packages/shared/src/constants/province-visuals.ts
@@ -12,34 +12,68 @@ export type ProvinceVisual = {
   credit: string;
 };
 
-function pexelsPhoto(id: number, width: number, height: number) {
-  return `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=${width}&h=${height}&fit=crop`;
-}
-
-function buildVisual(
+function buildVisualWiki(
   provinceId: number,
-  photoId: number,
+  fileName: string,
   majorCityEn: string,
   majorCityNp: string,
 ): ProvinceVisual {
+  const encodedFileName = encodeURIComponent(fileName);
+  const baseUrl = `https://commons.wikimedia.org/wiki/Special:FilePath/${encodedFileName}`;
+
   return {
     provinceId,
     majorCityEn,
     majorCityNp,
-    imageUrl: pexelsPhoto(photoId, 1280, 800),
-    imageUrlSmall: pexelsPhoto(photoId, 720, 460),
-    credit: `Pexels photo #${photoId}`,
+    imageUrl: `${baseUrl}?width=1280`,
+    imageUrlSmall: `${baseUrl}?width=720`,
+    credit: "Wikimedia Commons",
   };
 }
 
 export const provinceVisuals: ProvinceVisual[] = [
-  buildVisual(1, 2901209, "Biratnagar", "विराटनगर"),
-  buildVisual(2, 338515, "Janakpur", "जनकपुर"),
-  buildVisual(3, 2901209, "Kathmandu", "काठमाडौं"),
-  buildVisual(4, 2132180, "Pokhara", "पोखरा"),
-  buildVisual(5, 1457842, "Butwal", "बुटवल"),
-  buildVisual(6, 3225531, "Surkhet", "सुर्खेत"),
-  buildVisual(7, 417173, "Dhangadhi", "धनगढी"),
+  buildVisualWiki(
+    1,
+    "Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg", // Mount Everest
+    "Sagarmatha",
+    "सगरमाथा",
+  ),
+  buildVisualWiki(
+    2,
+    "Janaki_Mandir_of_Janakpur,_Nepal.jpg", // Janaki Temple
+    "Janakpur",
+    "जनकपुर",
+  ),
+  buildVisualWiki(
+    3,
+    "Boudha_Stupa_2018_04.jpg", // Boudhanath Stupa
+    "Kathmandu",
+    "काठमाडौं",
+  ),
+  buildVisualWiki(
+    4,
+    "Phewa_Lake_of_Pokhara_city.jpg", // Phewa Lake
+    "Pokhara",
+    "पोखरा",
+  ),
+  buildVisualWiki(
+    5,
+    "Lumbini_Mayadevi_Temple.jpg", // Lumbini - Birthplace of Buddha
+    "Lumbini",
+    "लुम्बिनी",
+  ),
+  buildVisualWiki(
+    6,
+    "Rara_Lake,_Mugu,_Nepal.jpg", // Rara Lake
+    "Rara Region",
+    "रारा क्षेत्र",
+  ),
+  buildVisualWiki(
+    7,
+    "Khaptad,_Khaptad_National_Park,_Nepal.jpg", // Khaptad National Park
+    "Khaptad",
+    "खप्तड",
+  ),
 ];
 
 export const provinceVisualsById = Object.fromEntries(
